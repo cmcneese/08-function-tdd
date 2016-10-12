bcd93e13745df0d1753d9c0a799ae183849b248a
Chris McNeese <christophermcneese@mac.com>
2016-10-12 15:29:37 -0500

    Generated Broccoli Browserify Project

--- app/blackjack/card-sum.js
+++ app/blackjack/card-sum.js
@@ -1,5 +1,13 @@
 import cardValue from './card-value';
 
 export default function (card1, card2) {
-
+  if (cardName === king) {
+    return 10;
+  }
+  if (cardName === queen) {
+    return 10;
+  }
+  if (cardName === jack) {
+    return 10;
+  }
 }
