4c573a211af74f3e5a0525d63b71215b22c04baf
Chris McNeese <christophermcneese@mac.com>
2016-11-14 16:57:42 -0600

    Test 10 complete

--- app/blackjack/card-sum.js
+++ app/blackjack/card-sum.js
@@ -1,9 +1,5 @@
 import cardValue from './card-value';
 
 export default function (card1, card2) {
-var x = cardSum
-
-function(card1) + function(card2)
-
-  }
+  return cardValue(card1) + cardValue(card2);
 }
--- app/blackjack/select-action.js
+++ app/blackjack/select-action.js
@@ -2,5 +2,13 @@ import shouldSplit from './should-split';
 import shouldHit from './should-hit';
 
 export default function (card1, card2) {
+  if (shouldSplit(card2, card1)) {
+    return 'split';
+  }
 
+  if (shouldHit(card1, card2)) {
+    return 'hit';
+  }
+
+  return 'stay';
 }
